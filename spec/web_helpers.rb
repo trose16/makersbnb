c1bceec5e4c3de431997e7b3afa5f74b09811196
Asuka Ochi <asukaochi@gmail.com>
2016-12-12 17:45:37 +0000

    Clicking on a listing takes you to the request page

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,5 +1,18 @@
 module WebHelpers
 
 # methods for testing here
+  def create_listing( name:        "Peacock Paradise Private Villa",
+                      description: "My home has peacocks and a pool.",
+                      price:       "50",
+                      available_from:  "12/12/2016",
+                      available_until: "22/04/2017" )
+    visit '/listings/new'
+    fill_in :name, with: name
+    fill_in :description, with: description
+    fill_in :price, with: price
+    fill_in :available_from, with: available_from
+    fill_in :available_until, with: available_until
+    click_button "List my Property"
+  end
 
 end
