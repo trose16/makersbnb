02ed8d7b0302b8772acf94e48e2770737469c630
Kim Wilson <k.wilson541@gmail.com>
2016-12-12 16:19:22 +0000

    Made Listings Model and Pages

--- spec/features/creating_listings_spec.rb
+++ spec/features/creating_listings_spec.rb
@@ -7,4 +7,18 @@ RSpec.feature 'Creating a Listing', :type => :feature do
 		expect(page.status_code).to eq 200
 	end
 
+	scenario 'user should be able to fill in a form' do
+		visit '/listings/new'
+		fill_in :name, with: "Peacock Paradise Private Villa"
+		fill_in :description, with: "My home has peacocks and a pool."
+		fill_in :price, with: "50"
+		fill_in :available_from, with: "12/12/2016"
+		fill_in :available_until, with: "22/04/2017"
+		click_button "List my Property"
+
+		expect(page.status_code).to eq 200
+		expect(page).to have_current_path('/listings')
+		expect(page).to have_content("Peacock Paradise Private Villa")
+	end
+
 end
