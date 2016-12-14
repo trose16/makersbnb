66eb07b372423d3dacf55a617bad3fc25d553fea
Kim Wilson <k.wilson541@gmail.com>
2016-12-14 12:03:34 +0000

    Got rid of unnecessary webhelpers and refactored specs appropriately

--- spec/features/log_out_spec.rb
+++ spec/features/log_out_spec.rb
@@ -5,7 +5,8 @@ feature 'logging out' do
 include WebHelpers
 
   scenario 'a logged in user logs out' do
-    sign_up
+    sign_up_owner
+    log_in
     click_button('Log out')
     expect(page.status_code).to eq 200
     expect(page).to have_selector(:link_or_button, 'Log in')
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,33 +1,38 @@
 module WebHelpers
 
-def sign_up
-  visit "/users/new"
-  fill_in "name", with: "Jane"
-  fill_in "email", with: "jane@email.com"
-  fill_in "password", with: "abcd1234"
-  fill_in "password_confirm", with: "abcd1234"
-  click_button "submit"
-end
+  @owner = {name: "Jane",
+            email: "jane@email.com",
+            password: "abcd1234",
+            password_confirm: "abcd1234"}
 
-def invalid_email_sign_up
-  visit "/users/new"
-  fill_in "name", with: "Jane"
-  fill_in "email", with: "not_an_email"
-  fill_in "password", with: "abcd1234"
-  fill_in "password_confirm", with: "abcd1234"
-  click_button "submit"
-end
+  @renter = {}
 
-def no_match_password_sign_up
-  visit "/users/new"
-  fill_in "name", with: "Jane"
-  fill_in "email", with: "jane@gmail.com"
-  fill_in "password", with: "abcd1234"
-  fill_in "password_confirm", with: "abcd"
-  click_button "submit"
-end
 
-# methods for testing here
+
+  def sign_up_owner(name: "Jane",
+                email: "jane@email.com",
+                password: "abcd1234",
+                password_confirm: "abcd1234")
+    visit "/users/new"
+    fill_in "name", with: name
+    fill_in "email", with: email
+    fill_in "password", with: password
+    fill_in "password_confirm", with: password_confirm
+    click_button "submit"
+  end
+
+  def sign_up_renter(name: "Sam",
+                      email: "sam@email.com",
+                      password: "efgh5678",
+                      password_confirm: "efgh5678")
+    visit "/users/new"
+    fill_in "name", with: name
+    fill_in "email", with: email
+    fill_in "password", with: password
+    fill_in "password_confirm", with: password_confirm
+    click_button "submit"
+  end
+
   def create_listing( name:        "Peacock Paradise Private Villa",
                       description: "My home has peacocks and a pool.",
                       price:       "50",
@@ -54,36 +59,13 @@ end
     click_button "List my Property"
   end
 
-def no_email_sign_up
-  visit "/users/new"
-  fill_in "name", with: "Jane"
-  fill_in "email", with: ""
-  fill_in "password", with: "abcd1234"
-  fill_in "password_confirm", with: "abcd1234"
-  click_button "submit"
-end
 
-def no_password_sign_up
-  visit "/users/new"
-  fill_in "name", with: "Jane"
-  fill_in "email", with: "jane@gmail.com"
-  fill_in "password", with: ""
-  fill_in "password_confirm", with: ""
-  click_button "submit"
-end
-
-def correct_log_in
-  visit '/sessions/new'
-  fill_in "email", with:  'jane@email.com'
-  fill_in "password", with: 'abcd1234'
-  click_button 'log-in'
-end
-
-def incorrect_log_in
-  visit '/sessions/new'
-  fill_in "email", with:  'jane@email.com'
-  fill_in "password", with: 'abcd4'
-  click_button 'log-in'
-end
+  def log_in(email: "jane@email.com", 
+              password: "abcd1234")
+    visit '/sessions/new'
+    fill_in "email", with:  email
+    fill_in "password", with: password
+    click_button 'log-in'
+  end
 
 end
