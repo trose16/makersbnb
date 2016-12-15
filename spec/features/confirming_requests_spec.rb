22bc8ad6172ed61e753fa583bfbab7e7494354b2
Kim Wilson <k.wilson541@gmail.com>
2016-12-15 16:27:59 +0000

    Users can see which bookings they have confirmed

--- spec/features/confirming_requests_spec.rb
+++ spec/features/confirming_requests_spec.rb
@@ -15,6 +15,16 @@ include WebHelpers
 			expect(page).to have_content('Request has been confirmed')
 	    end
 
+	    scenario "user can see bookings they have confirmed" do
+			make_request
+			log_out
+			log_in_owner
+			visit '/users/requests'
+			click_button('Confirm')
+			click_link('Go back to my requests')
+			expect(page).to have_content('Booking has been accepted')
+	    end
+
 	end
 
 	describe "deny" do
