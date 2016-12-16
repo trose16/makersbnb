976a2d6295bec7ead69a9bfd32abfd96c0918323
Kim Wilson <k.wilson541@gmail.com>
2016-12-16 12:40:57 +0000

    property owner can see the dates that their property has been requested

--- spec/features/viewing_requests_spec.rb
+++ spec/features/viewing_requests_spec.rb
@@ -9,6 +9,8 @@ include WebHelpers
 	    scenario "user can view the requests they have made" do
 	    	make_request
 	    	expect(page).to have_content("Peacock Paradise Private Villa")
+	    	expect(page).to have_content("Requested from: 2016-01-03")
+	    	expect(page).to have_content("Requested until: 2016-01-07")
 	    end
 
 	    scenario "user can view listing details of a property they have requested" do
@@ -28,6 +30,15 @@ include WebHelpers
 			visit '/users/requests'
 			expect(page).to have_content("Peacock Paradise Private Villa")
 			expect(page).to have_content("Requested by: Sam")
+		end
+
+		scenario "owner can see when their property has been requested from and until" do
+			make_request
+			log_out
+			log_in_owner
+			visit '/users/requests'
+			expect(page).to have_content("Requested from: 2016-01-03")
+	    	expect(page).to have_content("Requested until: 2016-01-07")
 		end
 
 	end
