369150ac806d92a2dcd8f809fa8f6a20d955efb8
Teako Warfield-Graham <teakowg@gmail.com>
2016-12-16 16:35:12 +0000

    Final css commits

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,4 @@
-class MakersBnb < Sinatra::Base
+	class MakersBnb < Sinatra::Base
 
 
 	get '/listings/new' do
@@ -33,23 +33,17 @@ class MakersBnb < Sinatra::Base
 
 	post '/listings/:id/request' do
 		@listing = Listing.first( id: params[:id] )
-		if @listing.user.id == current_user.id
-					flash.next[:notice] = "You cannot book a property you own."
-					redirect '/listings'
+
+		if @listing.check_availability( params[:book_from], params[:book_to] )
+			@booking = Booking.create(user_id: current_user.id, listing_id: params[:id], book_from: params[:book_from], book_to: params[:book_to] )
+			@booking.set_date_availability
+			@booking.save
+			redirect "/users/requests"
 		else
-				if @listing.check_availability( params[:book_from], params[:book_to] )
-					@booking = Booking.create(user_id: current_user.id,
-																		listing_id: params[:id],
-																		book_from: params[:book_from],
-																		book_to: params[:book_to] )
-					@booking.set_date_availability
-					@booking.save
-					redirect "/users/requests"
-				else
-					flash.keep[:notice] = "Sorry, the dates are not available."
-					redirect '/listings'
-				end
+			flash.keep[:notice] = "Sorry, the dates are not available."
+			redirect '/listings'
 		end
+
 	end
 
 	post '/listings/sort' do
